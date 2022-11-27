068c401aa06de896da5af2ec293bcd4dec3e9459
Mihai Ursu <ursu.mihai.mail@gmail.com>
2022-11-27 19:23:37 +0200

    Removed spacers

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,11 +4,9 @@ import styles from "../styles/Home.module.scss";
 export default function Home() {
   return (
     <div className={styles.container}>
-      <div className={styles.spacer} />
       <DynamicVideoButton>
         <div className={styles.video}></div>
       </DynamicVideoButton>
-      <div className={styles.spacer} />
     </div>
   );
 }
